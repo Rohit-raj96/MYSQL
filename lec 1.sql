fbc3ebcbb11dc30f84dcbcede2b441d877f3c997
Rohit-raj96 <54165862+Rohit-raj96@users.noreply.github.com>
2025-05-16 14:43:36 +0530

    Add files via upload

---
+++ lec 1.sql
@@ -0,0 +1,51 @@
+-- SQL  - Structred Query Language
+# MY SQL --Tool
+-- DBMS  - Developed to handle the large amount of data
+-- can  be Store, Added, Deleted, Updated, Received from one place
+
+-- RDBMS  -
+-- can BE Store, Added, Deleted, Updated, Retrieved From ONe PLace 
+
+-- Domain   (helatcare, food detection, ticket Vending system)
+-- Instance  (Object--  Class can have multiple instances/objects,--Mobile phone is example of class and it can be of oppo,vivo ete.  that is instance)
+--  Schema  --(DATa)
+-- KEys   (Identities)
+
+
+--  Sql Commands Categories 
+
+-- 1. Data Definition Language (DDl)
+-- 2. Data Manipulation Language (DML) 
+-- 3. Data Query Language (DQL)
+-- 4. Data Control Language (DCL) 
+-- 5. Transactional Control Language (TCL) 
+
+-- SQL Commands
+
+-- DDl - (Create, Alter, Drop, Truncate)  {DDL is Structre}
+#  database--->>- Table --->>-Column-->>  CREATE
+#  change 
+#  DROP -- >>  database| table and data also  delete full  y vanished
+#  TRuncate  -->> Structure remains same but 
+-- -- the  row deleted data deleted only
+
+
+-- DML - (Insert, Update, Delete)
+
+#   rows -->   In different row data INSERT
+#  Wronge data to change,    UPDATE
+#  remove the data that not requried  DELETE 
+#  Only ROW delete
+
+-- data analyst  Do not have access to data 
+#so can't be upadate delete 
+   
+-- DQL - (SELECT)
+#  Qurey --SELECT only this query
+# In real world we will use only this command  select select select
+
+
+-- DCL  (Grant,Revoke)
+#   database administrator gives permission
+
+-- TCL -- (commit, Rollback, Savepoint, Set transaction)
